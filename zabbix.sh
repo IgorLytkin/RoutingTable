1945db2c3ca33430e37cec0b90738866e91718db
Igor Lytkin <igor.lytkin.2020@ya.ru>
2026-07-01 06:02:00 +0700

    Add script

--- zabbix.sh
+++ zabbix.sh
@@ -4,3 +4,20 @@ UPDATE users
 SET passwd = '$2a$10$ZXIvHAEP2ZM.dLXTm6uPHOMVlARXX7cqjbhM6Fn0cANzkCQBWpMrS'
 WHERE username = 'Admin';
 SQL
+
+# Вариант 2 скрипт
+#!/usr/bin/env bash
+set -euo pipefail
+
+DB_CONTAINER="zabbix-mysql-server-1"
+DB_NAME="zabbix"
+ROOT_PASS="root_pwd"
+NEW_HASH='$2a$10$ZXIvHAEP2ZM.dLXTm6uPHOMVlARXX7cqjbhM6Fn0cANzkCQBWpMrS'
+
+sudo docker exec -i "$DB_CONTAINER" mysql -uroot -p"$ROOT_PASS" "$DB_NAME" <<SQL
+UPDATE users
+SET passwd = '$NEW_HASH'
+WHERE username = 'Admin';
+SQL
+
+echo "Admin password reset to zabbix"
